a95eca68e8861df12e24f25e60326ceacc50f13c
Josse Mandy <josse10@mmi-dev.unilim.fr>
2025-04-04 21:53:07 +0200

    Itération1: Css amélioré, besoin de dynamisation des textes du composant

---
+++ app/component/ite? carousel/script.js
@@ -0,0 +1,26 @@
+let templateFile = await fetch("./component/ite1/template.html");
+let template = await templateFile.text();
+let templateFile2 = await fetch("./component/ite1/templatecard.html");
+let template2 = await templateFile2.text();
+
+let List_ite1 = {};
+
+List_ite1.format = function (data) {
+  let html = template;
+  html += List_ite1.formatOneCard(data);
+  if (data.length == 0) {
+    content.innerHTML = "<p>Aucun film disponible pour le moment.</p>";
+  }
+  return html;
+};
+
+List_ite1.formatOneCard = function (data) {
+  let html = template2;
+
+  /* let truncated_name = data.name.length > 15 ? data.name.substring(0, 15) + "..." : data.name; */
+  html = html.replace("{{image_name}}", data.image_name);
+  html = html.replace("{{title}}", data.name);
+  return html;
+};
+
+export { List_ite1 };
