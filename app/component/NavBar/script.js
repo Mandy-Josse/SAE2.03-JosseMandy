a28a9a2c4129920e86f82519c71c6c86fc06ae58
Josse Mandy <josse10@mmi-dev.unilim.fr>
2025-04-10 14:22:20 +0200

    Liste profile affichée au mauvais endroit, travail de css en cours

--- app/component/NavBar/script.js
+++ app/component/NavBar/script.js
@@ -1,16 +1,19 @@
 let templateFile = await fetch("./component/NavBar/template.html");
 let template = await templateFile.text();
+let templateFileProfiles = await fetch("./component/NavBar/template.html");
+let templateProfiles = await templateFileProfiles.text();
 
 let NavBar = {};
-
 
 NavBar.formatOneCategory = function (data) {
   html = html.replace("{{category_id}}", data.id);
   html = html.replace("{{category_name}}", data.name);
 };
+
 NavBar.formatOneProfile = function (data) {
   return `<option value="${data.id}">${data.name}</option>`;
 };
+
 
 
 NavBar.format = function (hAbout, hProfile, data) {
@@ -23,14 +26,11 @@ NavBar.format = function (hAbout, hProfile, data) {
   data.forEach((profile) => {
     optionsHTML += NavBar.formatOneProfile(profile);
   });
-  
-  html = html.replace("{{profiles}}", optionsHTML);
 
+  html = html.replace("{{profiles}}", optionsHTML);
 
   return html;
 };
-
-
 
 //
 //
